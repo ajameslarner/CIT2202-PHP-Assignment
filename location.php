e445f35cc1e06aaf97a125f3a513e9649018ee9c
ajameslarner <ajameslarner@gmail.com>
2021-01-06 01:26:35 +0000

    autofill location search done

--- location.php
+++ location.php
@@ -1,10 +1,19 @@
 <?php
+//Include connection request
+include 'connect.php';
 
-$location = $_GET["location"];
+//Allocate form input variables
+//$location = $_GET["location"];
 $date = $_GET["date"];
 
+//prepared statement
+$stmt = $conn->prepare("SELECT * FROM hotels INNER JOIN locations ON locations.id = hotels.location_id AND locations.location LIKE ?");
+$stmt->execute([
+    "%".$_GET["location"]."%"
+]);
+$res=$stmt->fetchAll();
+$conn=NULL;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +23,21 @@ $date = $_GET["date"];
 </head>
 <body>
 <?php
-echo "<p>This is an output test {$location} & {$date}.</p>";
-
+if(isset($_GET["location"])) {
+    if (count($res)>0) {
+        foreach ($res as $r) {
+            printf("
+            <p>This is a test out</p> <br>
+            <p>Hotel name: {$r['name']}</p> <br>
+            <p>Hotel stars:  {$r['stars']}</p> <br>
+            <p>Hotel price: {$r['price']}</p> <br>
+            <p>Hotel location: {$r['location']}</p> <br>
+            ");
+        }
+    } else {
+        echo "<p>No results found!</p>";
+    }
+}
 ?>
 </body>
 </html>
